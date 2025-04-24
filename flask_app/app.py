164cb8c899e3c89c96ce2317f0ea1af69d61d2b0
spmcbth <machgiahan3624@gmail.com>
2025-04-24 20:08:09 +0700

    update

--- flask_app/app.py
+++ flask_app/app.py
@@ -288,7 +288,7 @@ def get_favorites():
         cursor = conn.cursor(dictionary=True)
         
         query = '''
-            SELECT id, message_id, conversation_id, content, message_type, created_at
+            SELECT id, conversation_id, content, message_type, created_at
             FROM favorites
             WHERE user_id = %s
             ORDER BY created_at DESC
